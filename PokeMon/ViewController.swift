36544bb10a2f8604e28f8f03889c23496ae9b4ee
Amrit Sapkota <MacGuru@amrits-macbook-pro.local>
2016-09-15 21:12:02 +0545

    Initial Commit

---
+++ PokeMon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokeMon
+//
+//  Created by Amrit Sapkota on 9/15/16.
+//  Copyright © 2016 Amrit Sapkota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
